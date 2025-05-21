08f36d2fa0c6418b4f2a9975c9dd6cf8975ad6f4
Isaac Thompson <risinginiris2017@gmail.com>
2025-05-21 14:49:50 +0800

    support multi json, bumped to 0.3.0.0

--- src/main/java/betteradvancements/proxy/ClientProxy.java
+++ src/main/java/betteradvancements/proxy/ClientProxy.java
@@ -1,6 +1,5 @@
 package betteradvancements.proxy;
 
-import betteradvancements.BetterAdvancements;
 import betteradvancements.advancements.CriterionDesc;
 import betteradvancements.config.ConfigHandler;
 import betteradvancements.handler.GuiOpenHandler;
@@ -8,12 +7,12 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import java.util.Objects;
-
+@SuppressWarnings("unused")
 public class ClientProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
+        CriterionDesc.getLoadedModIds();
         MinecraftForge.EVENT_BUS.register(new GuiOpenHandler());
         MinecraftForge.EVENT_BUS.register(new ConfigHandler());
     }
